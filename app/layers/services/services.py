2bf01ec39651d1c092e2deff1d3a7a3a3c3e7360
Gabriel Acosta <acostagabriel126@gmail.com>
2024-11-11 17:02:18 -0300

    Ejercicios obligatirios 1,2,3 - opcional 1

--- app/layers/services/services.py
+++ app/layers/services/services.py
@@ -1,15 +1,21 @@
 # capa de servicio/lógica de negocio
 
 from ..persistence import repositories
+from ..transport import transport
 from ..utilities import translator
 from django.contrib.auth import get_user
 
-def getAllImages(input=None):
+def getAllImages(input):
+    
     # obtiene un listado de datos "crudos" desde la API, usando a transport.py.
-    json_collection = []
-
+    json_collection = transport.getAllImages(input)
+    
     # recorre cada dato crudo de la colección anterior, lo convierte en una Card y lo agrega a images.
     images = []
+    for i in range (0, len(json_collection)):
+        print('voy a imprimir el valor de lista que estoy pasando: ', json_collection[i])
+        card = translator.fromRequestIntoCard(json_collection[i])
+        images.append(card)
 
     return images
 
